387158484c097e13325ffbace1467c98c2930b6d
Claire Antoine <cantoine85@gmail.com>
2016-08-10 01:03:34 +0200

    add front

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
-    @lyrics = @song.lyrics.split(",")
+    @lyrics = @song.lyrics
     # @lyrics = @song.lyrics.split("<br>\n<br>\n")
     @chords = ["Am", "Am", "Am", "Am", "Am" ]
   end
